4da65f13d575795dba3f8a0deb64a5c819afb3fd
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-03-07 18:05:50 +0200

    adding 3 sections

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+function show(){
+    document.getElementById('linkslist').classList.toggle('active');
+    document.getElementById('bar1').classList.toggle('active');
+    document.getElementById('bar2').classList.toggle('active');
+    document.getElementById('bar3').classList.toggle('active');
+}
