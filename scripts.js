ecd9b2d92059aa7e935a0d15e57fe2d372027ec6
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-03-11 15:30:18 +0200

    adding two pages

--- scripts.js
+++ scripts.js
@@ -5,3 +5,18 @@ function show(){
     document.getElementById('bar2').classList.toggle('active');
     document.getElementById('bar3').classList.toggle('active');
 }
+
+function Qshow(){
+    document.getElementById('Qdiscrption').classList.toggle('QdiscrptionShow');
+    document.getElementById('Qbtn').innerHTML = '-';
+}
+function Qshow2(){
+    document.getElementById('Qdiscrption2').classList.toggle('QdiscrptionShow');
+}
+function Qshow3(){
+    document.getElementById('Qdiscrption3').classList.toggle('QdiscrptionShow');
+}
+function Qshow4(){
+    document.getElementById('Qdiscrption4').classList.toggle('QdiscrptionShow');
+}
+
